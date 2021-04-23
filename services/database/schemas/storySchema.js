af3c9a110adf5c33d3fae8fd5337a2e25ecfdbea
JordonGoodsir <jordon.goodsir1@gmail.com>
2021-04-24 00:29:35 +1000

    story schema updated

--- services/database/schemas/storySchema.js
+++ services/database/schemas/storySchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Story = new mongoose.Schema({
   title: { type: String, required: true },
-  wordPrompts: { type: String, required: true },
+  wordPrompts: [{ type: String, required: true }],
   body: { type: String, required: true },
 });
 
